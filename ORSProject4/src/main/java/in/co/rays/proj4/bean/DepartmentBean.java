4e64ca36db878b67b1b413fbe5486952b0c0f6b3
abhi070717 <abhibhawsar066@gmail.com>
2026-02-25 02:42:50 +0530

    -

---
+++ ORSProject4/src/main/java/in/co/rays/proj4/bean/DepartmentBean.java
@@ -0,0 +1,42 @@
+package in.co.rays.proj4.bean;
+
+public class DepartmentBean extends BaseBean {
+
+	private String code;
+	private String name;
+	private String head;
+	private String location;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getHead() {
+		return head;
+	}
+
+	public void setHead(String head) {
+		this.head = head;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+}
