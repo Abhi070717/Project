ce5faf2e13e2ad4989f723050d58bb5a3191282b
abhi070717 <abhibhawsar066@gmail.com>
2026-02-25 23:47:06 +0530

    -

---
+++ ORSProject4/src/main/java/in/co/rays/proj4/bean/CertificateBean.java
@@ -0,0 +1,43 @@
+package in.co.rays.proj4.bean;
+
+import java.time.LocalDate;
+
+public class CertificateBean extends BaseBean {
+
+	private String certificateCode;
+	private String studentName;
+	private String courseName;
+	private LocalDate issueDate;
+
+	public String getCertificateCode() {
+		return certificateCode;
+	}
+
+	public void setCertificateCode(String certificateCode) {
+		this.certificateCode = certificateCode;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+	public LocalDate getIssueDate() {
+		return issueDate;
+	}
+
+	public void setIssueDate(LocalDate issueDate) {
+		this.issueDate = issueDate;
+	}
+}
